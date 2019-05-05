36fe8eb50acce1b94b157479058d1282b6b79181
Matteo Villa <matteo.villa@consulenti.chili.com>
2019-05-05 11:54:57 +0200

    frist

---
+++ src/PrimaryButton.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+
+require('./style.css')
+
+function PrimaryButton() {
+  return (
+    <div className="primaryButton">
+      Click Me
+    </div>
+  );
+}
+
+export default PrimaryButton;
